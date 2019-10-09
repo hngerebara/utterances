588903baf860b3769445de142ecba30df36c6608
Hope Ngerebara <hope.ngerebara@andela.com>
2019-10-09 09:03:37 +0100

    change

--- src/utterances-api.ts
+++ src/utterances-api.ts
@@ -1,2 +1,2 @@
 // export const UTTERANCES_API = 'http://localhost:5000';
-export const UTTERANCES_API = 'https://guides-comments.azurewebsites.net';
+export const UTTERANCES_API = 'https://guides-comments2.azurewebsites.net';
